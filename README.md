f234119b965fe92fbefa0ab2923bcf5b904dfb38
Kesuhi <110562470+Kesuhi@users.noreply.github.com>
2026-08-20 16:23:03 +0200

    Initial Fabric mod scaffold
    Client-side Fabric mod targeting Minecraft 26.1.2 / Fabric Loader 0.19.3 /
    Fabric API 0.155.2+26.1.2, using official Mojang mappings (Yarn is not yet
    published for 26.x). environment is set to "client" in fabric.mod.json so
    it never attempts to load server-side.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Skyblock Mob Highlighter
+
+A client-side Fabric mod that draws colored outlines on specific mobs while playing Hypixel Skyblock.
+
+## Setup
+
+For IDE setup instructions, see the [Fabric Documentation page](https://docs.fabricmc.net/develop/getting-started/creating-a-project#setting-up).
+
+## License
+
+This project is based on the official Fabric example mod template (CC0). See [LICENSE](LICENSE).
---
+++ src/main/java/com/kesuhi/skyblockhighlighter/SkyblockMobHighlighterClient.java
@@ -0,0 +1,16 @@
+package com.kesuhi.skyblockhighlighter;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SkyblockMobHighlighterClient implements ClientModInitializer {
+	public static final String MOD_ID = "skyblockhighlighter";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Skyblock Mob Highlighter initialized");
+	}
+}
